e8a8dbcca518c46d4ef318bd7c06bea89aa85b75
Dayna Pennock <125847956+DayPen@users.noreply.github.com>
2025-12-04 10:26:00 -0500

    layout-pages-comments

---
+++ frontend/src/Components/CreatePage.jsx
@@ -0,0 +1,15 @@
+/*
+Route: ("/create-user")
+
+if the user is new and wants to join, the user will
+click on the link on the login page to go to
+ROUTE:("/create-user"). this route will load a 
+new page that displays the same login component 
+with a different title and button message.
+Once the user is created, the form will 
+display a confirmation message. Also, the user 
+must navigate back to the login page on Route("/") 
+to log in with the new user. 
+
+**SEE PHOTOS ON B.B.
+*/
---
+++ frontend/src/Components/LoginPage.jsx
@@ -0,0 +1,13 @@
+/* the landing page will be a login form 
+expecting a username and password to enter.
+if credentials are valid, the page will 
+automatically navigate to ROUTE("/main"), 
+where the main GroceriesApp exists.
+if credentials are invalid, the form should 
+display a message that the username and password
+are incorrect. if the username does not exist 
+in the database, display a message stating that.
+tgere is also a link to ROUTE("/create-user") 
+to create a new user on the database. 
+SEE SCREENSHOTS ON BLACKBOARD...
+*/
